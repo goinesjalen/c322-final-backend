f7a9d5932196572208a84fd823cf9d6c3f395187
goinesjalen <goinesjalen@gmail.com>
2024-05-02 01:12:56 -0400

    implement mock testing

---
+++ src/test/java/edu/iu/habahram/c322final/model/CustomerTest.java
@@ -0,0 +1,42 @@
+package edu.iu.habahram.c322final.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+}
